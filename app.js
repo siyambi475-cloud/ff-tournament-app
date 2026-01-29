dd8096d8a509cdf0b04cd33038cb6bdb3a6c1b23
siyambi475-cloud <siyambi475@gmail.com>
2026-01-30 04:30:39 +0600

    app.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getFirestore, collection, onSnapshot } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD7G-O9kFFMSBOf1m23gtPKZ677q67xyEY",
@@ -7,14 +7,30 @@ const firebaseConfig = {
   projectId: "ffrnvm",
   storageBucket: "ffrnvm.firebasestorage.app",
   messagingSenderId: "578541384954",
-  appId: "1:578541384954:web:a7c20373284014b3244543",
-  measurementId: "G-H8MXRKSRRS"
+  appId: "1:578541384954:web:a7c20373284014b3244543"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const matchList = document.getElementById('match-list');
 
-console.log("Firebase Connected Successfully!");
-
-// ভবিষ্যতে এখান থেকেই আমরা টুর্নামেন্ট ডাটা লোড করবো।
+// ফায়ারবেস থেকে রিয়েল-টাইম ডাটা পড়া
+onSnapshot(collection(db, "matches"), (snapshot) => {
+    matchList.innerHTML = ""; // আগের সব লেখা মুছে নতুন ডাটা বসানো
+    snapshot.forEach((doc) => {
+        const match = doc.data();
+        matchList.innerHTML += `
+            <div class="match-card">
+                <div class="match-header">
+                    <span class="map">${match.map} - SOLO</span>
+                    <span class="status">LIVE</span>
+                </div>
+                <div class="match-body">
+                    <div class="info">Entry: <b>${match.fee} TK</b></div>
+                    <div class="info">Prize: <b>${match.prize} TK</b></div>
+                </div>
+                <button class="join-btn" onclick="alert('Joining: ${match.title}')">JOIN NOW</button>
+            </div>
+        `;
+    });
+});
